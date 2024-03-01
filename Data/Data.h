5c00dd1494b15df2cb20dbbb6fba9bb5bf850b00
BiscuitPrime <henri.nomico@gmail.com>
2024-03-01 12:49:29 +0100

    Added some comments

--- Data/Data.h
+++ Data/Data.h
@@ -3,7 +3,12 @@
 *	This header file will contain ALL the defined variables and data of the game.
 *	The reason is that the entire game is going to be console-based i.e. a lot of text. To simplify its creation, all the displayed text will be defined here :
 *	As such, any changes will immediately be reflected everywhere else in the game
-* 
+*	AS A RULE, WE SHOULD TRY TO HAVE AS MANY DEFINES MACRO HERE AS POSSIBLE TO STREAM LINE PROCESS IN OTHER GAME CLASSES
+*	Name rules :
+*	- Symbol		:	means a character or a list of characters
+*	- Generic_Name	:	means the generic name used in the game, informal and not bound to player-defined variables and parameters
+*	- Separator		:	means an element used to separates other words, contains the necessary space usually
+*	- Title			:	means that the element should be used at the beginning of a paragraphe or as an important starting line
 */
 
 // Lines and visual elements ------------------------
